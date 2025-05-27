62c6b9636f57647aaf8288f6318326ef0b731f24
Shivam Swaroop <shivamswaroop89@gmail.com>
2025-05-27 18:00:32 +0530

    In version 5, object-based useQuery implemented in CoinTable component

--- src/components/CoinTable/CoinTable.jsx
+++ src/components/CoinTable/CoinTable.jsx
@@ -1,7 +1,33 @@
+import { useEffect, useState } from "react";
+import { fetchCoinData } from "../../services/fetchCoinData";
+import { useQuery } from "@tanstack/react-query";
+
 const CoinTable = () => {
+  const [page, setPage] = useState(1);
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["coins", page],
+    queryFn: () => fetchCoinData(page, "usd"),
+    retry: 2, // Retry fetching data 2 times on failure
+    retryDelay: 1000, // Retry after 1 second
+    cacheTime: 1000 * 60 * 2, // Cache for 2 minutes
+  });
+
+  useEffect(() => {
+    console.log("Coin data fetched:", data);
+  }, [data]);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    return <div>Error: {error.message}</div>;
+  }
+
   return (
-    <div>CoinTable</div>
-  )
-}
+    <>
+      Coin Table <button onClick={() => setPage(page + 1)}>Click</button> {page}
+    </>
+  );
+};
 
 export default CoinTable;
--- src/services/fetchCoinData.js
+++ src/services/fetchCoinData.js
@@ -1,8 +1,9 @@
 import axiosInstance from "../utils/axiosInstance"
 
-export async function fetchCoinData(){
+export async function fetchCoinData(page = 1, currency = 'usd'){
+    const perPage = 10; // Default value for perPage
 try {
-    const response = await axiosInstance.get('/coins/markets?vs_currency=usd');
+    const response = await axiosInstance.get(`/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${perPage}&page=${page}`);
     console.log("Coin data fetched successfully:", response);
     return response;
  }
