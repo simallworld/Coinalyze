c520f4ae9080217fa4cb823f445c4d43bede231a
Shivam Swaroop <shivamswaroop89@gmail.com>
2025-05-27 19:35:37 +0530

    Cointable component done will update some functionality tommorow

--- src/components/CoinTable/CoinTable.jsx
+++ src/components/CoinTable/CoinTable.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import { fetchCoinData } from "../../services/fetchCoinData";
 import { useQuery } from "@tanstack/react-query";
 
@@ -12,21 +12,84 @@ const CoinTable = () => {
     cacheTime: 1000 * 60 * 2, // Cache for 2 minutes
   });
 
-  useEffect(() => {
-    console.log("Coin data fetched:", data);
-  }, [data]);
-
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
   if (isError) {
     return <div>Error: {error.message}</div>;
   }
 
   return (
-    <>
-      Coin Table <button onClick={() => setPage(page + 1)}>Click</button> {page}
-    </>
+    <div className="my-5 flex flex-col items-center justify-center gap-5 w-[80vw] mx-auto">
+      <div className="w-full bg-orange-600 flex flex-row justify-center text-black py-4 px-4 font-semibold items-center rounded-sm">
+        {/* Header of the table */}
+        <div className="basis-[35%]">Coin</div>
+        <div className="basis-[25%]">Price</div>
+        <div className="basis-[20%]">24h Change</div>
+        <div className="basis-[20%]">Market Cap</div>
+      </div>
+
+      <div className="flex flex-col w-[80vw] mx-auto">
+        {isLoading && <div>Loading...</div>}
+        {data &&
+          data.map((coin) => {
+            return (
+              <div
+                key={coin.id}
+                className="w-full bg-transparent text-white flex py-4 px-2 font-semibold items-center justify-between cursor-pointer"
+              >
+                {/*Coin row*/}
+                <div className="flex flex-row items-center justify-start gap-3 basis-[35%]">
+                  {/*Coin name and image*/}
+                  <div className="w-[5rem] h-[5rem]">
+                    <img
+                      src={coin.image}
+                      alt="coinSymbol"
+                      className="w-full h-full"
+                    />
+                    {/*Coin image*/}
+                  </div>
+                  <div className="flex flex-col">
+                    {/*Coin name*/}
+                    <span className="text-2xl">{coin.name}</span>
+                    <span className="text-xl text-gray-400">
+                      {coin.symbol.toUpperCase()}
+                    </span>
+                  </div>
+                </div>
+
+                <div className="basis-[25%]">
+                  {/*Coin Price*/}
+                  {coin.high_24h}
+                </div>
+                <div className="basis-[20%]">
+                  {/*Coin price_change_24h*/}
+                  {coin.price_change_24h}
+                </div>
+                <div className="basis-[20%]">
+                  {/*Coin market_cap*/}
+                  {coin.market_cap}
+                </div>
+              </div>
+            );
+          })}
+      </div>
+
+      {/* Buttons */}
+
+      <div className="flex gap-4 justify-center items-center">
+        <button
+          disabled={page === 1}
+          onClick={() => setPage(page - 1)}
+          className="btn btn-primary btn-wide text-white text-2xl"
+        >
+          Prev
+        </button>
+        <button
+          onClick={() => setPage(page + 1)}
+          className="btn btn-secondary btn-wide text-white text-2xl"
+        >
+          Next
+        </button>
+      </div>
+    </div>
   );
 };
 
--- src/services/fetchCoinData.js
+++ src/services/fetchCoinData.js
@@ -4,8 +4,7 @@ export async function fetchCoinData(page = 1, currency = 'usd'){
     const perPage = 10; // Default value for perPage
 try {
     const response = await axiosInstance.get(`/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${perPage}&page=${page}`);
-    console.log("Coin data fetched successfully:", response);
-    return response;
+    return response.data;
  }
   catch (error) {
     console.error("Error fetching coin data:", error); 
