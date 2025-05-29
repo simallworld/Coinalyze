3ece5d6a9118506008909f30110a2d4627dc38b1
Shivam Swaroop <shivamswaroop89@gmail.com>
2025-05-29 16:25:32 +0530

    updated the issue- CurrentContext was not imported in the App.jsx Component but firxed now...

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import { useState } from "react";
 import "./App.css";
 import Home from "./pages/Home";
+import { CurrencyContext } from "./context/CurrencyContext";
 
 function App() {
   const [currency, setCurrency] = useState("usd");
 
   return (
     <>
-      <CurrencyContext.Provider value={{currency, setCurrency}}>
+      <CurrencyContext.Provider value={{ currency, setCurrency }}>
         <Home />
       </CurrencyContext.Provider>
     </>
